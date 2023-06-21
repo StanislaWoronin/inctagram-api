7d2c88061840eaa8d0c43ccb2727202508ac9f9f
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2023-06-21 18:44:23 +0200

    update

---
+++ apps/auth/src/auth.controller.reciever.ts
@@ -0,0 +1,17 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { RegistrationDto } from '../../../libs/users/dto';
+import { Commands } from '../../../libs/shared';
+import { UserFacade } from '../../../libs/users/application-services';
+import { RegistrationResponse } from '../../../libs/users/response';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly userFacade: UserFacade) {}
+
+  @MessagePattern({ cmd: Commands.Registration })
+  async registration(data: RegistrationDto): Promise<RegistrationResponse> {
+    console.log('auth');
+    return await this.userFacade.commands.registrationUser(data);
+  }
+}
