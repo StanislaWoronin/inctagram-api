e3c34eaec31859f977686524ec75b53be67ccc8c
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2023-06-27 22:27:29 +0200

    UPD: updated function results

--- apps/auth/src/testing.repository.ts
+++ apps/auth/src/testing.repository.ts
@@ -2,7 +2,6 @@ import { Connection, Model } from 'mongoose';
 import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
 import { UserAggregate, UsersDocument } from '../../../libs/users/schema';
-import { logLevel } from '@nestjs/microservices/external/kafka.interface';
 
 @Injectable()
 export class TestingRepository {
